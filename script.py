64fe93ce370c560dea6f0a28c1445848168fd886
Ryan Bacastow <rbacastow@clarity-us.com>
2017-12-13 13:58:20 -0600

    so many changes, so little time.

--- script.py
+++ script.py
@@ -1,67 +1,49 @@
 import boto3
 from datetime import datetime, timedelta
+global instances
+from collections import defaultdict
+
+instances = []
+defdict=defaultdict(list)
+
+dynamo=boto3.resource("dynamodb", region_name="us-east-2")
+table = dynamo.Table("dynamite-dev")
+response = table.scan()
+data = response['Items']
+
+def add_instance_type(name):
+    if name not in instances:
+        instances.append(name)
+        #eval_instance(name)
+
+a = list()
+counter = 0
+def eval_instance(instance_name):
+    for adict in data:
+        if adict.get('type') == instance_name:
+            inst_count=0.0
+            times = {k: v for k, v in adict.items() if k == "start_dttm" or k == "delete_dttm"}
+            g = [datetime.strptime(date, "%Y-%m-%d %H:%M:%S.%f") for date in times.values()]
+            print(g)
+    a = a[counter]
+    counter +=1
+    a.append((g[0]-g[1]))
+    print(a)
 
 def main():
     table_name = "instance_avgs"
-    global num 
+    global num
     num = 0
-    instances=['vertica','spark','ldp','talend','linux_python','pyspark']
-    
-    vertica = []
-    spark = []
-    ldp = []
-    talend = []
-    linux_python = []
-    pyspark = []
-    
-    dynamo=boto3.resource("dynamodb", region_name="us-east-2")
-    table = dynamo.Table("dynamite-dev")
-    response = table.scan()
-    data = response['Items']
-    
-    
+    #instances=['vertica','spark','ldp','talend','linux_python','pyspark']
     for adict in data:
-        if adict.get('type') == instances[0]:
-            name_vertica = 'vertica'
-            vertica_count=0.0
-            times = {k: v for k, v in adict.items() if k == "start_dttm" or k == "delete_dttm"}
-            x = [datetime.strptime(date, "%Y-%m-%d %H:%M:%S.%f") for date in times.values()]
-            vertica.append((x[0]-x[1]))
-        elif adict.get('type') == instances[1]:
-            name_spark = 'spark'
-            spark_count=0.0
-            times = {k: v for k, v in adict.items() if k == "start_dttm" or k == "delete_dttm"}
-            x = [datetime.strptime(date, "%Y-%m-%d %H:%M:%S.%f") for date in times.values()]
-            spark.append((x[0]-x[1]))
-        elif adict.get('type') == instances[2]:
-            name_ldp = 'ldp'
-            ldp_count=0.0
-            times = {k: v for k, v in adict.items() if k == "start_dttm" or k == "delete_dttm"}
-            x = [datetime.strptime(date, "%Y-%m-%d %H:%M:%S.%f") for date in times.values()]
-            ldp.append((x[0]-x[1]))
-        elif adict.get('type') == instances[3]:
-            name_talend = 'talend'
-            talend_count=0.0
-            times = {k: v for k, v in adict.items() if k == "start_dttm" or k == "delete_dttm"}
-            x = [datetime.strptime(date, "%Y-%m-%d %H:%M:%S.%f") for date in times.values()]
-            talend.append((x[0]-x[1]))
-        elif adict.get('type') == instances[4]:
-            name_linux_python = 'linux_python'
-            linux_python_count=0.0
-            times = {k: v for k, v in adict.items() if k == "start_dttm" or k == "delete_dttm"}
-            x = [datetime.strptime(date, "%Y-%m-%d %H:%M:%S.%f") for date in times.values()]
-            linux_python.append((x[0]-x[1]))
-        elif adict.get('type') == instances[5]:
-            name_pyspark = 'pyspark'
-            pyspark_count=0.0
-            times = {k: v for k, v in adict.items() if k == "start_dttm" or k == "delete_dttm"}
-            x = [datetime.strptime(date, "%Y-%m-%d %H:%M:%S.%f") for date in times.values()]
-            pyspark.append((x[0]-x[1]))
-        else:
-             print("Can't determine type.")
-                
+        add_instance_type(adict.get('type'))
+    for instance_name in instances:
+        eval_instance(instance_name)
+    """
     table_out=dynamo.Table(table_name)
-    
+
+    def converter():
+
     for t in vertica:
         seconds=float(t.total_seconds())
         mins=round(seconds/60,3)
@@ -69,7 +51,7 @@ def main():
         vertica_avg =round(vertica_count/len(vertica),2)
         vertica_avg = str(vertica_avg)
     print('Avg Time for a {} instance: {} minutes.'.format(name_vertica,round(vertica_count/len(vertica),2)))
-    
+
     table_out.put_item(
         Item={
             'Instance_Type':name_vertica,
@@ -87,15 +69,15 @@ def main():
             'Average_Time': spark_avg,
         })
     print('Avg Time for a {} instance: {} minutes.'.format(name_spark, round(spark_count/len(spark),2)))
-    
+
     for t in ldp:
         seconds=float(t.total_seconds())
         mins=round(seconds/60,3)
         ldp_count+=mins
         ldp_avg=round(ldp_count/len(ldp),2)
         ldp_avg=str(ldp_avg)
-    print('Avg Time for a {} instance: {} minutes.'.format(name_ldp, round(ldp_count/len(ldp),2)))   
-    
+    print('Avg Time for a {} instance: {} minutes.'.format(name_ldp, round(ldp_count/len(ldp),2)))
+
     table_out.put_item(
         Item={
             'Instance_Type':'LDP',
@@ -113,7 +95,7 @@ def main():
         'Average_Time':talend_avg,
         })
     print('Avg Time for a {} instance: {} minutes.'.format(name_talend, round(talend_count/len(talend),2)))
-    
+
     for t in linux_python:
         seconds=float(t.total_seconds())
         mins=round(seconds/60,3)
@@ -126,7 +108,7 @@ def main():
             'Average_Time': linux_python_avg,
         })
     print('Avg Time for a {} instance: {} minutes.'.format(name_linux_python, round(linux_python_count/len(linux_python),2)))
-    
+
     for t in pyspark:
         seconds=float(t.total_seconds())
         mins=round(seconds/60,3)
@@ -139,6 +121,6 @@ def main():
             'Average_Time': pyspark_avg,
         })
     print('Avg Time for a {} instance: {} minutes.'.format(name_pyspark, round(pyspark_count/len(pyspark),2)))
-    
+"""
 if __name__ == "__main__":
     main()
