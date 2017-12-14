5a553c5cccc15cfd91b8bd4f61899b5f422b14de
Sam Miller <smiller@clarity-us.com>
2017-12-14 09:03:51 -0600

    hey

--- script.py
+++ script.py
@@ -10,120 +10,46 @@ dynamo=boto3.resource("dynamodb", region_name="us-east-2")
 table = dynamo.Table("dynamite-dev")
 response = table.scan()
 data = response['Items']
-masterlist=[]
+table_name = "instance_avgs"
+table_out=dynamo.Table(table_name)
+count = 0
 
 def add_instance_type(name):
     if name not in instances:
         instances.append(name)
 
-        #eval_instance(name)
-
-
 def eval_instance(instance_name):
+    a=[]
     for adict in data:
         if adict.get('type') == instance_name:
-            inst_count=0.0
+            instance_name_test = instance_name
             times = {k: v for k, v in adict.items() if k == "start_dttm" or k == "delete_dttm"}
             g =[datetime.strptime(date, "%Y-%m-%d %H:%M:%S.%f") for date in times.values()]
             g =g[0]-g[1]
-            a=[]
             a.append(g)
-            a.append(adict.get('type'))
-            print(a)
-            #masterlist.append()
-    #print(masterlist)
+    converter(a, instance_name_test)
+
+def converter(a, instance_name_test):
+    count = 0
+    for t in a:
+        seconds=float(t.total_seconds())
+        mins=round(seconds/60,3)
+        count+=mins
+    avg=round(count/len(a),2)
+    str_avg= str(avg)
+    table_out.put_item(
+        Item={
+            'Instance_Type': instance_name_test,
+            'Average_Time': str_avg,
+        })
+    print('The average time for a '+ instance_name_test+' instance: '+str_avg)
 
 def main():
-    table_name = "instance_avgs"
-    global num
-    num = 0
-    #instances=['vertica','spark','ldp','talend','linux_python','pyspark']
     for adict in data:
         add_instance_type(adict.get('type'))
     for instance_name in instances:
         eval_instance(instance_name)
-    """
-    table_out=dynamo.Table(table_name)
 
-    def converter():
-
-    for t in vertica:
-        seconds=float(t.total_seconds())
-        mins=round(seconds/60,3)
-        vertica_count+=mins
-        vertica_avg =round(vertica_count/len(vertica),2)
-        vertica_avg = str(vertica_avg)
-    print('Avg Time for a {} instance: {} minutes.'.format(name_vertica,round(vertica_count/len(vertica),2)))
-
-    table_out.put_item(
-        Item={
-            'Instance_Type':name_vertica,
-            'Average_Time': vertica_avg,
-        })
-    for t in spark:
-        seconds=float(t.total_seconds())
-        mins=round(seconds/60,3)
-        spark_count+=mins
-        spark_avg=round(spark_count/len(spark),2)
-        spark_avg=str(spark_avg)
-    table_out.put_item(
-        Item={
-            'Instance_Type':'Spark',
-            'Average_Time': spark_avg,
-        })
-    print('Avg Time for a {} instance: {} minutes.'.format(name_spark, round(spark_count/len(spark),2)))
-
-    for t in ldp:
-        seconds=float(t.total_seconds())
-        mins=round(seconds/60,3)
-        ldp_count+=mins
-        ldp_avg=round(ldp_count/len(ldp),2)
-        ldp_avg=str(ldp_avg)
-    print('Avg Time for a {} instance: {} minutes.'.format(name_ldp, round(ldp_count/len(ldp),2)))
-
-    table_out.put_item(
-        Item={
-            'Instance_Type':'LDP',
-            'Average_Time': ldp_avg,
-        })
-    for t in talend:
-        seconds=float(t.total_seconds())
-        mins=round(seconds/60,3)
-        talend_count+=mins
-        talend_avg=round(talend_count/len(talend),2)
-        talend_avg=str(talend_avg)
-    table_out.put_item(
-        Item={
-        'Instance_Type':'Talend',
-        'Average_Time':talend_avg,
-        })
-    print('Avg Time for a {} instance: {} minutes.'.format(name_talend, round(talend_count/len(talend),2)))
-
-    for t in linux_python:
-        seconds=float(t.total_seconds())
-        mins=round(seconds/60,3)
-        linux_python_count+=mins
-        linux_python_avg=round(linux_python_count/len(linux_python),2)
-        linux_python_avg=str(linux_python_avg)
-    table_out.put_item(
-        Item={
-            'Instance_Type':'Linux Python',
-            'Average_Time': linux_python_avg,
-        })
-    print('Avg Time for a {} instance: {} minutes.'.format(name_linux_python, round(linux_python_count/len(linux_python),2)))
-
-    for t in pyspark:
-        seconds=float(t.total_seconds())
-        mins=round(seconds/60,3)
-        pyspark_count+=mins
-        pyspark_avg=round(pyspark_count/len(pyspark),2)
-        pyspark_avg=str(pyspark_avg)
-    table_out.put_item(
-        Item={
-            'Instance_Type':'Pyspark',
-            'Average_Time': pyspark_avg,
-        })
-    print('Avg Time for a {} instance: {} minutes.'.format(name_pyspark, round(pyspark_count/len(pyspark),2)))
-"""
+    
 if __name__ == "__main__":
     main()
